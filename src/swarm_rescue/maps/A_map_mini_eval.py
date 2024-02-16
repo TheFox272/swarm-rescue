25c988437836aba19f2e7509ba93eaad01dbf01e
ProgrammingLouis <louis.programer@gmail.com>
2024-02-16 16:39:43 +0100

    updates mini_eval map

--- src/swarm_rescue/maps/A_map_mini_eval.py
+++ src/swarm_rescue/maps/A_map_mini_eval.py
@@ -13,7 +13,7 @@ from spg.utils.definitions import CollisionTypes
 
 from spg_overlay.entities.drone_abstract import DroneAbstract, drone_collision_wall, drone_collision_drone
 from spg_overlay.entities.rescue_center import RescueCenter, wounded_rescue_center_collision
-from spg_overlay.entities.sensor_disablers import ZoneType, NoGpsZone, srdisabler_disables_device
+from spg_overlay.entities.sensor_disablers import ZoneType, NoGpsZone, srdisabler_disables_device, KillZone, NoComZone
 from spg_overlay.entities.wounded_person import WoundedPerson
 from spg_overlay.gui_map.closed_playground import ClosedPlayground
 from spg_overlay.gui_map.map_abstract import MapAbstract
@@ -36,15 +36,21 @@ class MapMiniEval(MapAbstract):
         self._rescue_center = RescueCenter(size=(210,155))
         self._rescue_center_pos = ((637,-451), 0)
 
-        self._no_gps_zone = NoGpsZone(size=(400, 500))
-        self._no_gps_zone_pos = ((-190, 0), 0)
+        self._no_gps_zone = NoGpsZone(size=(400, 600))
+        self._no_gps_zone_pos = ((-130, 0), 0)
 
-        self._wounded_persons_pos = []
+        self._kill_zone = KillZone(size=(100, 200))
+        self._kill_zone_pos = ((-540, 60), 0)
+
+        self._no_com_zone = NoComZone(size=(500, 300))
+        self._no_com_zone_pos = ((600, 0), 0)
+
+        self._wounded_persons_pos = [(67,-506), (-716,-504), (65,-254), (-488,-251), (-131,61), (303,63), (676,69), (-405,130), (267,225), (-720,505), (710,513), (54,512)]
         self._number_wounded_persons = len(self._wounded_persons_pos)
         self._wounded_persons: List[WoundedPerson] = []
 
         orient = random.uniform(-math.pi, math.pi)
-        self._drones_pos = [((295, 118), orient)]
+        self._drones_pos = [((640, -350), orient), ((600, -350), orient), ((560, -350), orient), ((680, -350), orient), ((640, -300), orient), ((600, -300), orient), ((560, -300), orient), ((680, -300), orient)]
         self._number_drones = len(self._drones_pos)
         self._drones: List[DroneAbstract] = []
 
@@ -70,6 +76,12 @@ class MapMiniEval(MapAbstract):
 
         if ZoneType.NO_GPS_ZONE in self._zones_config:
             playground.add(self._no_gps_zone, self._no_gps_zone_pos)
+
+        if ZoneType.KILL_ZONE in self._zones_config:
+            playground.add(self._kill_zone, self._kill_zone_pos)
+
+        if ZoneType.NO_COM_ZONE in self._zones_config:
+            playground.add(self._no_com_zone, self._no_com_zone_pos)
 
         # POSITIONS OF THE WOUNDED PERSONS
         for i in range(self._number_wounded_persons):
